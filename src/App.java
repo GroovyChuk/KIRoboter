60b987cbf62aee22be4460f79ba5442fc912773b
GroovyChuk <alasdair333@hotmail.com>
2017-12-20 15:35:14 +0100

    Color sensor added

--- src/App.java
+++ src/App.java
@@ -6,6 +6,8 @@ import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.chassis.Chassis;
@@ -18,7 +20,8 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		Wheel wheel1 = WheeledChassis.modelWheel(Motor.A, 23).offset(-86);
 		Wheel wheel2 = WheeledChassis.modelWheel(Motor.D, 23).offset(86);
-		EV3UltrasonicSensor ultraSonic = new EV3UltrasonicSensor(LocalEV3.get().getPort("S1"));
+		EV3UltrasonicSensor ultraSonic = new EV3UltrasonicSensor(SensorPort.S1);
+		EV3ColorSensor color = new EV3ColorSensor(SensorPort.S4);
 		GraphicsLCD g = LocalEV3.get().getGraphicsLCD();
 		
 		Chassis chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
@@ -30,16 +33,20 @@ public class App {
 		pilot.setAngularSpeed(100);
 
 		SampleProvider distance = ultraSonic.getMode("Distance");
-		float[] sample = new float[distance.sampleSize()];
-		distance.fetchSample(sample, 0);
+		SampleProvider light = color.getMode("RGB");
+		float[] sampleD = new float[distance.sampleSize()];
+		float[] sampleL = new float[light.sampleSize()];
+		
+		distance.fetchSample(sampleD, 0);
+		distance.fetchSample(sampleL, 0);
 		
 		for (int i = 0; i < 5; i++) {
 			Button.waitForAnyPress();
-			distance.fetchSample(sample, 0);
-			g.drawString("Distanz: " + sample[0], 5, 20*i + 20, 0);
+			distance.fetchSample(sampleD, 0);
+			g.drawString("Distanz: " + sampleD[0], 5, 20*i + 20, 0);
 		}
 
-			
+
 		
 //		for (int i = 0; i < 5; i++) {
 //			pilot.travel(100); // cm
