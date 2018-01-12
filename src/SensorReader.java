4b6970769c92aaada2d2c96bcb456eb12e4a3068
Kalaman <oemer.kalaman@hotmail.de>
2018-01-12 13:23:57 +0100

    Add files via upload

---
+++ src/SensorReader.java
@@ -0,0 +1,42 @@
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
+public class SensorReader {
+	private SampleProvider distanceProvider;
+	private SampleProvider lightProvider;
+	private EV3UltrasonicSensor ultraSonicSensor;
+	private EV3ColorSensor colorSensor;
+	private EV3Console console;
+	
+	private float[] sampleDistance;
+	private float[] sampleLight;
+	
+	public SensorReader (EV3Console ev3console) {
+		ultraSonicSensor = new EV3UltrasonicSensor(SensorPort.S1);
+		colorSensor = new EV3ColorSensor(SensorPort.S4); 
+		
+		distanceProvider = ultraSonicSensor.getMode("Distance");
+		lightProvider = colorSensor.getMode("RGB");
+		
+		sampleDistance = new float[distanceProvider.sampleSize()];
+		sampleLight = new float[lightProvider.sampleSize()];
+		
+		console = ev3console;
+	}
+	
+	public float[] readLight () {
+		lightProvider.fetchSample(sampleLight,0);
+		return sampleLight;
+	}
+	
+	public float[] readDistance () {
+		distanceProvider.fetchSample(sampleDistance,0);
+		return sampleDistance;
+	}
+	
+	public void logSensorData () {
+		console.log("Distance: " + readDistance()[0] + " Meter\nLight: " + readLight()[0]);
+	}
+}
